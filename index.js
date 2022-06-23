981efbb3df96e9830305b68e847b983cbfaf2430
Vadim Igumnov <vad.igumnoff@gmail.com>
2022-06-23 10:22:45 +0300

    feat: add language switch functionality

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-let nav = document.querySelector('nav');
-let navLinks = document.querySelectorAll('.nav__item')
+const navigationBlock = document.querySelector("nav");
+const navigationLinks = document.querySelectorAll(".nav__item");
+const languageSwitcher = document.querySelector(".lang-switch__vertical");
+const languageSwitcherItems = document.querySelectorAll(".lang-switch__item");
+const textOnEnglish = document.querySelectorAll("[lang=eng]");
+const textOnRussian = document.querySelectorAll("[lang=ru]");
 
-nav.addEventListener('click', handleNavigationLinks)
+navigationBlock.addEventListener("click", handleNavigationLinks);
+languageSwitcher.addEventListener("click", handleLanguageSwitcher);
+
+let currentLanguage = 'ENG';
 
 function handleNavigationLinks(e) {
-  if (!e.target.classList.contains('nav__item')) return 
+  if (!e.target.parentNode.classList.contains("nav__item")) return;
 
-  for (link of navLinks) {
-    if (link.classList.contains('nav__item_active')) {
-      link.classList.remove('nav__item_active');
+  for (link of navigationLinks) {
+    if (link.classList.contains("nav__item_active")) {
+      link.classList.remove("nav__item_active");
     }
   }
-  e.target.classList.add('nav__item_active')
+  e.target.parentNode.classList.add("nav__item_active");
+}
+
+function handleLanguageSwitcher(e) {
+  if (!e.target.classList.contains("lang-switch__item") ||
+      e.target.classList.contains("lang-switch__item_active")
+      ) return
+
+  for (lang of languageSwitcherItems) {
+    if (lang.classList.contains('lang-switch__item_active')) {
+      lang.classList.remove('lang-switch__item_active');
+    }
+  }
+  e.target.classList.add('lang-switch__item_active');
+  currentLanguage = e.target.innerHTML;
+  changePageLanguage();
+}
+
+function changePageLanguage() {
+  if (currentLanguage === 'ENG') {
+    textOnRussian.forEach(item => item.classList.add('hidden'))
+    textOnEnglish.forEach(item => item.classList.remove('hidden'))
+  }
+  if (currentLanguage === 'RU') {
+    textOnEnglish.forEach(item => item.classList.add('hidden'))
+    textOnRussian.forEach(item => item.classList.remove('hidden'))
+  }
 }
