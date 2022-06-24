471d2850250379262485086db1ddfe9a788e924b
Vadim Igumnov <vad.igumnoff@gmail.com>
2022-06-24 14:52:20 +0300

    feat: implement burger menu

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const languageSwitcher = document.querySelector(".lang-switch__vertical");
 const languageSwitcherItems = document.querySelectorAll(".lang-switch__item");
 const textOnEnglish = document.querySelectorAll("[lang=eng]");
 const textOnRussian = document.querySelectorAll("[lang=ru]");
+const burgerMenu = document.querySelector(".hamburger");
+const navigationLinksBlock = document.querySelector('.nav__links');
 
 navigationBlock.addEventListener("click", handleNavigationLinks);
 languageSwitcher.addEventListener("click", handleLanguageSwitcher);
+burgerMenu.addEventListener("click", handleHamburgerMenu);
 
 let currentLanguage = 'ENG';
 
@@ -19,6 +22,7 @@ function handleNavigationLinks(e) {
     }
   }
   e.target.parentNode.classList.add("nav__item_active");
+  toggleHamburgerMenu()
 }
 
 function handleLanguageSwitcher(e) {
@@ -44,5 +48,19 @@ function changePageLanguage() {
   if (currentLanguage === 'RU') {
     textOnEnglish.forEach(item => item.classList.add('hidden'))
     textOnRussian.forEach(item => item.classList.remove('hidden'))
+  }
+}
+
+function handleHamburgerMenu() {
+  toggleHamburgerMenu()
+}
+
+function toggleHamburgerMenu() {
+  if (!burgerMenu.classList.contains('hamburger_active')) {
+    burgerMenu.classList.add('hamburger_active');
+    navigationLinksBlock.classList.add('nav__links_active');
+  } else {
+    burgerMenu.classList.remove('hamburger_active');
+    navigationLinksBlock.classList.remove('nav__links_active');
   }
 }
